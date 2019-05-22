fe5d5fdb99b6904b3bd8b9b3e02e482901d07ca8
Jared Schwantz <c.jared.schwantz@gmail.com>
2019-05-22 01:31:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 // add solution here
-function theBeatlesPlay(musicians, instruments)
-{
+function theBeatlesPlay(musicians, instruments) {
   var lines = [];
   
   for (var i = 0; i < musicians.length; i++) {
     lines.push(musicians[i]+' plays '+instruments[i]);
   }
   return lines;
+}
+
+function johnLennonFacts(facts) {
+  
 }
