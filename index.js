62cd74591460b62d37c98628b4ce6acb54cdbd24
Jared Schwantz <c.jared.schwantz@gmail.com>
2019-05-22 01:29:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function theBeatlesPlay(musicians, instruments)
 {
   var lines = [];
   
+  var i;
   for (i = 0; i < musicians.length; i++) {
     lines.push(musicians[i]+' plays '+instruments[i]);
   }
